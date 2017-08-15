2d14baf672286f97fe6ba8b6eeac61b3014d6407
Fumitaka Tokumitsu <toku345@gmail.com>
2017-08-15 10:54:48 +0900

    chap4.1 max

--- src/grokking_algorithms_cljs/chap4.cljs
+++ src/grokking_algorithms_cljs/chap4.cljs
@@ -18,3 +18,13 @@
   (if (empty? list)
     0
     (+ 1 (count (rest list)))))
+
+
+(defn maximum
+  ([x] x)
+  ([x y] (if (> x y) x y))
+  ([x y & more] (apply maximum (cons (maximum x y) more))))
+
+(defn mymax
+  [xs]
+  (apply maximum xs))
--- test/grokking_algorithms_cljs/chap4_test.cljs
+++ test/grokking_algorithms_cljs/chap4_test.cljs
@@ -1,12 +1,17 @@
 (ns grokking_algorithms_cljs.chap4-test
   (:require [cljs.test :refer-macros [deftest is]]
-            [grokking_algorithms_cljs.chap4 :refer [sum count]]))
+            [grokking_algorithms_cljs.chap4 :refer [sum count mymax]]))
 
 (deftest return-sum-total
   (is (= 10 (sum [1 2 3 4])))
-  (is (= 1 (sum [1])))
-  (is (= 0 (sum []))))
+  (is (= 1  (sum [1])))
+  (is (= 0  (sum []))))
 
 (deftest return-number-of-passed-list-elements
   (is (= 5 (count [1 2 3 4 5])))
   (is (= 0 (count []))))
+
+(deftest return-mux-number
+  (is (= 5  (mymax [1 5 3 4 2])))
+  (is (= 1  (mymax [1])))
+  (is (= -1 (mymax [-100 -1 -10]))))
